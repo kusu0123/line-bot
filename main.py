95fdf43b55d18257c96b8007b16b5ad99262bee2
楠大輝 <daiki@kusuMacBook-Air.local>
2025-06-28 01:12:18 +0900

    supabase.pyの追加等

--- main.py
+++ main.py
@@ -12,25 +12,20 @@ load_dotenv()
 
 LINE_BOT_API=LineBotApi(os.environ["ACCESS_TOKEN"])
 handler=WebhookHandler(os.environ["CHANNEL_SECRET"])
-app = FastAPI()
 
-# Fetch variables
+
 USER = os.getenv("USER")
 PASSWORD = os.getenv("PASSWORD")
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
 DBNAME = os.getenv("DBNAME")
 
-# Construct the SQLAlchemy connection string
 SUPABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
-# Create the SQLAlchemy engine
 engine = create_engine(SUPABASE_URL)
-# If using Transaction Pooler or Session Pooler, we want to ensure we disable SQLAlchemy client side pooling -
-# https://docs.sqlalchemy.org/en/20/core/pooling.html#switching-pool-implementations
-# engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
-# Test the connection
+app = FastAPI()
+
 try:
     with engine.connect() as connection:
         print("Connection successful!")
---
+++ repository/supabase.py
@@ -0,0 +1,40 @@
+import os
+import psycopg
+# データを入れ込む関数
+def insert_package(conn_str:str,package_name:str):
+    print(f"\n--- INSERTクエリの実行 ('{package_name}' を追加) ---")
+    try:
+        with psycopg.connect(conn_str) as conn:
+                with conn.cursor() as cur:
+                    sql_query = """
+                WITH inserted_package AS (
+                    INSERT INTO package(name) VALUES (%s) RETURNING id
+                )
+                INSERT INTO record(package_id, date)
+                SELECT id, CURRENT_DATE FROM inserted_package;
+                """
+                cur.execute(sql_query, (package_name,))
+    except psycopg.Error as e:
+        print(f"データベースエラーが発生しました: {e}")
+
+# データを取ってくる関数 
+def select_package_record(conn_str:str):
+    print("--- SELECTクエリの実行 ---")
+    try:
+        with psycopg.connect(conn_str) as conn:
+            with conn.cursor() as cur:
+                sql_query = "SELECT package.name,MAX(CASE WHEN record_ranked rn=2 THEN record_ranked.date END) AS m, COUNT(record.package_id) FROM package JOIN record ON package.id=record.package_id GROUP BY m DNSC;"
+                
+                cur.execute(sql_query)
+                
+                results = cur.fetchall()
+                
+                if results:
+                    print("取得結果:")
+                    for row in results:
+                        print(f"  忘れ物名: {row[0]}, 前々回忘れた日付: {row[1]}, 忘れた回数: {row[2]}")
+                else:
+                    print("データが見つかりませんでした。")
+                    
+    except psycopg.Error as e:
+        print(f"データベースエラーが発生しました: {e}")
