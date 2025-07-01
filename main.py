ed44f7617724563844e48f7f74d2bf42183719f7
楠大輝 <daiki@kusuMacBook-Air.local>
2025-07-01 19:00:24 +0900

    修正

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from fastapi import FastAPI, Request, BackgroundTasks, Header
 from dotenv import load_dotenv
 from starlette.exceptions import HTTPException
 from sqlalchemy import create_engine
-import os
-
+app=FastAPI()
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
-
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Renderが自動でPORTを渡す
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+   
 from repository import supabase
 
 load_dotenv()
@@ -31,7 +32,6 @@ PSYCOPG_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=re
 
 engine = create_engine(SQLALCHEMY_URL)
 
-app = FastAPI()
 
 try:
     with engine.connect() as connection:
